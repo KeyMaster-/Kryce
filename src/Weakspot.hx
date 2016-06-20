c0e0cb6b5c67acb59027fd4f315fb2b03c5d59b7
Tilman Schmidt <AccessKeymaster@gmx.net>
2016-06-20 11:00:06 +0200

    Test traditional controls. Committing in case I want them later.

--- src/Weakspot.hx
+++ src/Weakspot.hx
@@ -14,7 +14,6 @@ class Weakspot extends Visual {
     var move_radius:Float;
 
     var dyn_shape:DynamicShape;
-    var collision_vis:Visual;
 
     public function new(_x:Float, _y:Float, _r:Float, _move_radius:Float, _phys_engine:ShapePhysics, ?_options:VisualOptions) {
         if(_options == null) _options = {};
@@ -25,19 +24,8 @@ class Weakspot extends Visual {
         move_radius = _move_radius;
 
         dyn_shape = new DynamicShape(new Circle(_x, _y, _r), new Vector(0, 0));
-        // _phys_engine.dynamics.push(dyn_shape);
         _phys_engine.weakspot = dyn_shape;
         _phys_engine.callbacks.set(dyn_shape.shape, oncollision);
-
-        _options.name = 'weakspot_collision';
-        _options.geometry = Luxe.draw.circle({
-            r:_r,
-            x:0,
-            y:0
-        });
-
-        collision_vis = new Visual(_options);
-        collision_vis.add(new ShapeComponent(dyn_shape));
 
         _options.name = 'weakspot';
         _options.geometry = Luxe.draw.circle({
@@ -58,16 +46,7 @@ class Weakspot extends Visual {
     }
 
     override public function update(_dt:Float) {
-        dyn_shape.vel.copy_from(pos);
-        dyn_shape.vel.subtract(dyn_shape.shape.position);
-        if(dyn_shape.vel.length < 5) {
-            dyn_shape.shape.position.copy_from(pos);
-            dyn_shape.vel.set_xy(0, 0);
-        }
-        else {
-            dyn_shape.vel.length = 400;
-        }
-        // dyn_shape.vel.length = Math.min(dyn_shape.vel.length, 300);
+        pos.copy_from(dyn_shape.shape.position);
 
     }
 
@@ -84,9 +63,8 @@ class Weakspot extends Visual {
         }
 
         relative_pos.length = Maths.clamp(relative_pos.length, 0, 1);
-        pos.copy_from(relative_pos);
-        pos.multiplyScalar(move_radius);
-        pos.add(base_pos);
+        dyn_shape.vel.copy_from(relative_pos);
+        dyn_shape.vel.length *= 400;
     }
 
     inline function trunc_abs(_v:Float, _epsilon:Float):Float {
