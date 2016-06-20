6dcba225e5119cc8f820c9cdd7a8c65ab9a0bc19
Tilman Schmidt <AccessKeymaster@gmx.net>
2016-06-20 10:54:50 +0200

    Experimental dragged weakspot

--- src/Weakspot.hx
+++ src/Weakspot.hx
@@ -13,7 +13,8 @@ class Weakspot extends Visual {
 
     var move_radius:Float;
 
-    var shape:Circle;
+    var dyn_shape:DynamicShape;
+    var collision_vis:Visual;
 
     public function new(_x:Float, _y:Float, _r:Float, _move_radius:Float, _phys_engine:ShapePhysics, ?_options:VisualOptions) {
         if(_options == null) _options = {};
@@ -21,26 +22,53 @@ class Weakspot extends Visual {
         base_pos = new Vector(_x, _y);
         relative_pos = new Vector(0, 0);
 
-        move_radius = _move_radius;        
+        move_radius = _move_radius;
 
-        _options.name = 'Weakspot';
+        dyn_shape = new DynamicShape(new Circle(_x, _y, _r), new Vector(0, 0));
+        // _phys_engine.dynamics.push(dyn_shape);
+        _phys_engine.weakspot = dyn_shape;
+        _phys_engine.callbacks.set(dyn_shape.shape, oncollision);
+
+        _options.name = 'weakspot_collision';
         _options.geometry = Luxe.draw.circle({
             r:_r,
             x:0,
             y:0
         });
+
+        collision_vis = new Visual(_options);
+        collision_vis.add(new ShapeComponent(dyn_shape));
+
+        _options.name = 'weakspot';
+        _options.geometry = Luxe.draw.circle({
+            r:_r / 2,
+            x:0,
+            y:0
+        });
+
+        if(_options.color != null) _options.color.a = 0.5;
         super(_options);
 
         pos.copy_from(base_pos);
-
-        shape = new Circle(_x, _y, _r);
-        _phys_engine.statics.push(shape);
-        _phys_engine.callbacks.set(shape, oncollision);
     }
 
     override public function onreset() {
         axis_change(0, 0);
         axis_change(1, 0);
+    }
+
+    override public function update(_dt:Float) {
+        dyn_shape.vel.copy_from(pos);
+        dyn_shape.vel.subtract(dyn_shape.shape.position);
+        if(dyn_shape.vel.length < 5) {
+            dyn_shape.shape.position.copy_from(pos);
+            dyn_shape.vel.set_xy(0, 0);
+        }
+        else {
+            dyn_shape.vel.length = 400;
+        }
+        // dyn_shape.vel.length = Math.min(dyn_shape.vel.length, 300);
+
     }
 
     function oncollision(coll:ShapeCollision) {
@@ -59,7 +87,6 @@ class Weakspot extends Visual {
         pos.copy_from(relative_pos);
         pos.multiplyScalar(move_radius);
         pos.add(base_pos);
-        shape.position.copy_from(pos);
     }
 
     inline function trunc_abs(_v:Float, _epsilon:Float):Float {
